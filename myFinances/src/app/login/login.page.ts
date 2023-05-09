9e44e8e8fce3c7cb2d19fe18e3c0c5dcb205bb46
alaia <alaiatorre2001@gmail.com>
2023-05-09 13:39:49 +0200

    login egiten du

--- myFinances/src/app/login/login.page.ts
+++ myFinances/src/app/login/login.page.ts
@@ -2,14 +2,32 @@ import { Component, OnInit } from '@angular/core';
 import { Router } from '@angular/router';
 import {AngularFireDatabase} from '@angular/fire/compat/database';
 import {AngularFireAuth} from '@angular/fire/compat/auth';
+import { AuthService } from '../services/auth.service';
+
 @Component({
   selector: 'app-login',
   templateUrl: './login.page.html',
   styleUrls: ['./login.page.scss'],
 })
 export class LoginPage implements OnInit {
-  user: any = {};
-  constructor(private router: Router, private afDB: AngularFireDatabase, private auth: AngularFireAuth) { }
+
+  email: string = '';
+  password: string = '';
+
+
+  constructor(private router:Router, private authService: AuthService) { }
+
+  checkLog() {
+    this.authService.login(this.email, this.password)
+      .then(() => {
+        // Navigate to the home page
+        console.log('Logged in!');
+      })
+      .catch((error) => {
+        // Handle login error
+        alert("The email or password you entered is incorrect.");
+      });
+  }
 
   ngOnInit() {
   }
@@ -17,4 +35,5 @@ export class LoginPage implements OnInit {
   home() {
     this.router.navigateByUrl('/tabs/tab1');
   }
+
 }
--- myFinances/src/app/services/auth.service.ts
+++ myFinances/src/app/services/auth.service.ts
@@ -1,18 +1,18 @@
 import { Injectable } from '@angular/core';
 import { HttpClient } from '@angular/common/http';
 import { NgForm } from '@angular/forms';
+import { AngularFireAuth } from '@angular/fire/compat/auth';
+
 
 
 @Injectable({
   providedIn: 'root'
 })
-export class registerService {
+export class AuthService {
 
-  constructor(private http: HttpClient) { }
+  constructor(private afAuth: AngularFireAuth) { }
 
-  onSubmit(form:NgForm){
-    if(form.invalid){
-      return;
-    }
+  login(email: string, password: string) {
+    return this.afAuth.signInWithEmailAndPassword(email, password);
   }
 }
